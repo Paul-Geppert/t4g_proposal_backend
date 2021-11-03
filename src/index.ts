b2e2b317c103ac55188e363ae4a7b840925cafd4
Paul Geppert <paul.geppert@student.hpi.de>
2021-11-03 13:32:57 +0100

    feat(): Switch to choose pdf generation framework

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,22 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import MarkdownPDF from "markdown-pdf";
 
-import * as template from "../templates/general";
+import * as pdf from "../templates/pdf";
 import * as xmlTemplate from "../templates/easyOnline";
 import { fillTemplate, fillXMLTemplate } from "./controllers/templateController";
 
 const prettyMdPdf = require("pretty-markdown-pdf");
 const fs = require('fs').promises;
 
-const app: Application = express();
+// one of MarkdownPdf and PrettyMarkdownPdf
+let MARKDOWN_CONVERTER_TO_USE = "PrettyMarkdownPdf";
+
 const PORT = 3003;
-const OUTPUTPATH_MD= "./tmp/doc.md";
+const OUTPUTPATH_MD = "./tmp/doc.md";
 const OUTPUTPATH_PDF = "./tmp/doc.pdf";
 const OUTPUTPATH_XML = "./tmp/easyOnline.xml";
+
+const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,24 +33,34 @@ app.get(
 app.post(
     "/",
     (req: Request, res: Response) => {
-        return fillXMLTemplate(xmlTemplate.default, req.body)
-            .then((templ) => { return fs.writeFile(OUTPUTPATH_XML, templ); })
-            .then(() => console.log())
-            .then(() => { return fillTemplate(template.default, req.body) })
-            .then((templ) => { return fs.writeFile(OUTPUTPATH_MD, templ) })
-            .then(() => {
-                prettyMdPdf.convertMd({ markdownFilePath: OUTPUTPATH_MD, outputFilePath: OUTPUTPATH_PDF })
-                res.send();
-            })
-        // return fillTemplate(template.default, req.body)
-        //     .then((template) => {
-        //         MarkdownPDF({
-        //             cssPath: "../assets/github-markdown.css"
-        //         }).from.string(template).to(OUTPUTPATH, () => {
-        //             console.log("Finished creation of PDF");
-        //             res.status(200).send();
-        //         });
-        //     });
+        if (MARKDOWN_CONVERTER_TO_USE === "MarkdownPdf") {
+            return fillXMLTemplate(xmlTemplate.default, req.body)
+                .then((templ) => { return fs.writeFile(OUTPUTPATH_XML, templ); })
+                .then(() => { return fillTemplate(pdf.default, req.body) })
+                .then((template) => {
+                    MarkdownPDF({
+                        cssPath: "../assets/github-markdown.css"
+                    }).from.string(template).to(OUTPUTPATH_PDF, () => {
+                        console.log("Finished creation of PDF");
+                        res.status(200).send();
+                    });
+                });
+        }
+
+        else if (MARKDOWN_CONVERTER_TO_USE === "PrettyMarkdownPdf") {
+            return fillXMLTemplate(xmlTemplate.default, req.body)
+                .then((templ) => { return fs.writeFile(OUTPUTPATH_XML, templ); })
+                .then(() => { return fillTemplate(pdf.default, req.body) })
+                .then((templ) => { return fs.writeFile(OUTPUTPATH_MD, templ) })
+                .then(() => {
+                    prettyMdPdf.convertMd({ markdownFilePath: OUTPUTPATH_MD, outputFilePath: OUTPUTPATH_PDF })
+                    res.send();
+                })
+        }
+
+        else {
+            res.status(500).send("Could not find a matching Markdown to PDF converter");
+        }
     }
 );
 
